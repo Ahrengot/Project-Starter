903e07eafe4025a8f289102e9ac4d8666f0c2b48
Jens Ahrengot Boddum <ahrengot@gmail.com>
2015-12-17 12:54:29 +0100

    Update welcome view

--- src/scripts/components/view-welcome/welcome.js
+++ src/scripts/components/view-welcome/welcome.js
@@ -3,12 +3,25 @@ import Styles from './welcome.css'
 export default React.createClass({
 	render() {
 		return (
-			<article className={ Styles.container + " view container-fluid" }>
+			<article className="view container-fluid">
 				<div className="view-content">
-					<header className="text-center">
+					<header>
 						<h2>Welcome!</h2>
+						<h3 style={{ marginBottom: '2rem' }}>
+							<small>You now have the following available</small>
+						</h3>
 					</header>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi praesentium, harum unde tempora, distinctio eligendi vero, ratione veniam ad nesciunt aliquid iste temporibus ut. Dolorem error ut, ea. Esse, cum.</p>
+					<ul className={ Styles['feature-list'] }>
+						<li>Webpack</li>
+						<li>React.js</li>
+						<li>Redux + Dev tools</li>
+						<li>React Router</li>
+						<li>PostCSS loader for JS components</li>
+						<li>Hot module replacement</li>
+						<li>SVG Sprites</li>
+						<li>Browserify (easy debugging on multiple devices)</li>
+						<li>Bootstrap 4 alpha (Sass version)</li>
+					</ul>
 				</div>
 			</article>
 		)
